f52ff896f6bb328f3558adc1bcf294fe94a114b9
Ilya G Goldberg <igg@cathilya.org>
2013-06-13 13:17:10 -0400

    append crontab to existing

--- Server/setup/05-Ubuntu12-Install-Final.sh
+++ Server/setup/05-Ubuntu12-Install-Final.sh
@@ -12,9 +12,23 @@ date=`date '+%F'`
 
 # set up lighttpd and start it
 sudo mv /etc/lighttpd/lighttpd.conf /etc/lighttpd/lighttpd.conf-OLD
-sudo cp "${ROOT_DIR}/setup/lighttpd.conf /etc/lighttpd/
-mkdir "${ROOT_DIR}/var
+sudo cp "${ROOT_DIR}/setup/lighttpd.conf" /etc/lighttpd/
+mkdir "${ROOT_DIR}/var"
 sudo service lighttpd start
 
-# cron
-sudo crontab "${ROOT_DIR}/setup/crontab
+# cron - append to root's pre-existing crontab
+sudo bash -c "crontab -l > ${ROOT_DIR}/setup/crontab-OLD"
+sudo bash -c "cat ${ROOT_DIR}/setup/crontab >> ${ROOT_DIR}/setup/crontab-OLD"
+sudo bash -c "mv ${ROOT_DIR}/setup/crontab-OLD ${ROOT_DIR}/setup/crontab"
+sudo crontab "${ROOT_DIR}/setup/crontab"
+
+# generate server key
+openssl genrsa -F4 -out "${ROOT_DIR}/priv/BBD.pem" 1024
+openssl rsa -in "${ROOT_DIR}/priv/BBD.pem" -out "${ROOT_DIR}/priv/BBD-pub.pem" -pubout
+chmod 400 "${ROOT_DIR}/priv/BBD.pem"
+chmod a-w "${ROOT_DIR}/priv/BBD-pub.pem"
+# get gateway conf
+# This is a test gateway (server won't start without one):
+cp "${ROOT_DIR}/setup/GW_test.conf" "${ROOT_DIR}/priv/GW.conf"
+chmod 400 "${ROOT_DIR}/priv/GW.conf"
+# use bin/BBD-invite.pl to create the first member
