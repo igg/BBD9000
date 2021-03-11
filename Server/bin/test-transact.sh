751b1a8b5d136db8ad48afea76e93e5e1e130fda
Ilya Goldberg <iggie@mac.com>
2021-03-11 12:19:33 -0800

    adding initial shell script for transaction test

---
+++ Server/bin/test-transact.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# first param on command line is the gateway configuration file (e.g. BBDC_GW_test.conf)
+# second param is track data file (1 track per line, track1 followed by track2)
+# both files may contain comment lines beginning with #
+while IFS=$' ' read -r CC_URL x_cpversion x_login x_market_type x_device_type x_tran_key; do
+  [[ "$CC_URL" =~ ^#.*$ ]] && continue
+  printf "%b\n" "CC_URL<${CC_URL}>"
+  printf "%b\n" "x_cpversion<${x_cpversion}>"
+  printf "%b\n" "x_login<${x_login}>"
+  printf "%b\n" "x_market_type<${x_market_type}>"
+  printf "%b\n" "x_device_type<${x_device_type}>"
+  printf "%b\n" "x_tran_key<${x_tran_key}>"
+done < "$1"
+
+tracks=()
+while read -r line; do
+  [[ "$line" =~ ^#.*$ ]] && continue
+  tracks+=("$line")
+done < "$2"
+
+printf "%b\n" "track1<${tracks[0]}>"
+printf "%b\n" "track2<${tracks[1]}>"
+
+x_type=AUTH_ONLY
+x_track2=tracks[1]
+x_amount=1.0
+
